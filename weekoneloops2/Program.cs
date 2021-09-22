608083803dabf15a207b1c04d6574edbe1da57a5
midhorner <horner.andrew.e@gmail.com>
2021-09-22 15:46:20 -0400

    messages project

--- weekoneloops2/Program.cs
+++ weekoneloops2/Program.cs
@@ -6,7 +6,65 @@ namespace weekoneloops2
     {
         static void Main(string[] args)
         {
-            for (int i = 1; i <= 100; i++)
+
+            string[] msgs = new string[3];
+            bool on = true;
+
+            while (on) { 
+                Console.WriteLine("Would you like to leave a message, retrieve a message, or quit?");
+                string ans = Console.ReadLine();
+
+                if (ans == "m")
+                {
+                    for (int i = 0; i < msgs.Length; i++)
+                    {           
+                        
+                        if (msgs[i] == null)
+                        {
+                            Console.WriteLine("Leave your message!");
+                            msgs[i] = Console.ReadLine();
+                            Console.WriteLine($"Your message number is {i}");
+                            Console.WriteLine("Would you like to leave another");
+                            string ans2 = Console.ReadLine();
+                            if (ans2 == "y")
+                            {
+                                if (i == 2)
+                                {
+                                    Console.WriteLine("Your mailbox is Full!");
+                                }
+                                else 
+                                { 
+                                    Console.WriteLine("ok!"); 
+                                }
+                                
+                            }
+                            else
+                            {
+                                i = msgs.Length;
+                            }
+                        }
+                        else
+                        {
+                            Console.WriteLine("Your mailbox is full.");
+                            i = msgs.Length;
+                        }
+                    
+                    }
+                }
+                if(ans == "r")
+                {
+                    Console.WriteLine("Which message would like to retreive: 0-3");
+                    int refNum = Convert.ToInt32(Console.ReadLine());
+                    Console.WriteLine(msgs[refNum]);
+                }
+                if(ans == "q") 
+                { 
+                    on = false; 
+                }
+            }
+
+
+            /*for (int i = 1; i <= 100; i++)
             {
                 string FizzBuzz = "";
                 if (i % 3 == 0)
@@ -22,18 +80,18 @@ namespace weekoneloops2
                     FizzBuzz = Convert.ToString(i);
                 }
                 Console.WriteLine(FizzBuzz);
-            }
+            }*/
             /*for (int i = 1; i <= 100; i++)
             {
-                if(i%5 ==0 && i%3 == 0)
+                if (i % 5 == 0 && i % 3 == 0)
                 {
                     Console.WriteLine("FizzBuzz");
                 }
-                else if(i%5 == 0)
+                else if (i % 5 == 0)
                 {
                     Console.WriteLine("Buzz");
                 }
-                else if (i%3 == 0)
+                else if (i % 3 == 0)
                 {
                     Console.WriteLine("Fizz");
                 }
