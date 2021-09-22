8e7643b58f9828f7983babcb7505a2628c9bb0ee
midhorner <horner.andrew.e@gmail.com>
2021-09-22 09:55:34 -0400

    loops

---
+++ weekoneloops2/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace weekoneloops2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
