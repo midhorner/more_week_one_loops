678a8d86dbc469115da78cd85bb26cb4a1d46594
midhorner <horner.andrew.e@gmail.com>
2021-09-22 11:20:05 -0400

    fizzbuzz

--- weekoneloops2/Program.cs
+++ weekoneloops2/Program.cs
@@ -6,7 +6,43 @@ namespace weekoneloops2
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            for (int i = 1; i <= 100; i++)
+            {
+                string FizzBuzz = "";
+                if (i % 3 == 0)
+                {
+                    FizzBuzz = FizzBuzz + "Fizz";
+                }
+                if (i % 5 == 0)
+                {
+                    FizzBuzz = FizzBuzz + "Buzz";
+                }
+                if (i % 5 != 0 && i % 3 != 0)
+                {
+                    FizzBuzz = Convert.ToString(i);
+                }
+                Console.WriteLine(FizzBuzz);
+            }
+            /*for (int i = 1; i <= 100; i++)
+            {
+                if(i%5 ==0 && i%3 == 0)
+                {
+                    Console.WriteLine("FizzBuzz");
+                }
+                else if(i%5 == 0)
+                {
+                    Console.WriteLine("Buzz");
+                }
+                else if (i%3 == 0)
+                {
+                    Console.WriteLine("Fizz");
+                }
+                else
+                {
+                    Console.WriteLine(i);
+                }
+            }*/
+
         }
     }
 }
